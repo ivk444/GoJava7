c3e0d11dfb3b594cccf1ddbbfe6d882ae36052e5
Ivan Kolesnyk <ivk444@gmail.com>
2017-05-04 23:52:03 +0300

    Initial commit of the project

---
+++ src/JavaCore/Module1/Task011/Task011.java
@@ -0,0 +1,14 @@
+package JavaCore.Module1.Task011;
+
+import java.util.Scanner;
+
+public class Task011 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter Your name:");
+        String name = new Scanner(System.in).nextLine();
+        System.out.println("Hello, " + name);
+
+    }
+}
---
+++ src/JavaCore/Module2/Task012/Task012.java
@@ -0,0 +1,11 @@
+package JavaCore.Module2.Task012;
+
+
+
+public class Task012 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
