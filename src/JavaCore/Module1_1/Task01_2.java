25ea7f8bdd09b9689648c56770b4a0bb07ad5459
Ivan Kolesnyk <ivk444@gmail.com>
2017-05-17 15:24:06 +0300

    Restructurise GoJava7

--- src/JavaCore/Module1_1/Task01_2.java
+++ src/JavaCore/Module1_1/Task01_2.java
@@ -1,10 +1,24 @@
 package JavaCore.Module1_1;
 
-
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Task01_2 {
 
     public static void main(String[] args) {
+
+//        System.out.println("введите желаемую длину массива целых чисел");
+//        int i = new Scanner(System.in).nextInt();
+        int mass[] = new int[5];
+
+        for (int n = 0; n < 5; n++) {
+            System.out.println("введите целое число - " + n + "-й элемент массива");
+            mass[n] = new Scanner(System.in).nextInt();
+        }
+        System.out.println("введенный массив" + Arrays.toString(mass));
+
+        Arrays.sort(mass);
+        System.out.println("отсортированный массив" + Arrays.toString(mass));
 
     }
 
